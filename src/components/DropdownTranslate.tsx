b0092f37ff9eb93ff66c3454fb5f0662298c037d
Thuat PC <thuatpc@vmogroup.com>
2024-10-04 18:04:05 +0700

    init i18n

---
+++ src/components/DropdownTranslate.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DropdownTranslate = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default DropdownTranslate;
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,30 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import './satoshi.css';
+import global_en from './common/i18n/en/en.json';
+import global_vi from './common/i18n/vi/vi.json';
+import i18next from 'i18next';
+import { I18nextProvider } from 'react-i18next';
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: 'en',
+  resources: {
+    en: {
+      global: global_en,
+    },
+    vi: {
+      global: global_vi,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+    <I18nextProvider i18n={i18next}>
+      <Router>
+        <App />
+      </Router>
+    </I18nextProvider>
+  </React.StrictMode>,
 );
